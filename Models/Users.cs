2c9d83ef228ddd5d19391cf50a83de97e742aed8
Keana Cobarde <keanacobarde@gmail.com>
2024-02-06 18:43:26 -0600

    Creating user class

--- Models/Users.cs
+++ Models/Users.cs
@@ -1,6 +1,18 @@
-﻿namespace rare.Models
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace rare.Models
 {
     public class Users
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Bio { get; set; }
+        public string Username { get; set; }
+        public string Password  { get; set; }
+        public string ProfileImgUrl { get; set; }
+        public string CreatedOn { get; set; }
+        public string Active { get; set; }
     }
 }
